563c14338c92c326a7d0bec48a63824ea046093f
Viezas <dxviezas@gmail.com>
2022-06-10 13:21:53 +0200

    [Design] Improve connection page design

--- app/src/main/java/com/webstart/teamup/ConnexionActivity.java
+++ app/src/main/java/com/webstart/teamup/ConnexionActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 
 public class ConnexionActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,13 +14,16 @@ public class ConnexionActivity extends AppCompatActivity {
     }
 
     public void goToHome(View view) {
-        //vérification des informations de connexion
-        //aller à la Home Page
         Intent home = new Intent(this,HomeActivity.class);
         startActivity(home);
     }
 
-    public void goToGoogleConnxion(View view) {
+    public void goToGoogleConnexion(View view) {
         //créer un compte avec les informations google de la personne
+    }
+
+    public void goToSignUp(View view) {
+        Intent signUp = new Intent(this,InscriptionActivity.class);
+        startActivity(signUp);
     }
 }
