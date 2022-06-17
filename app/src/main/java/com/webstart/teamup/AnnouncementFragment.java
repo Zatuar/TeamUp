b87342234df360730671adcaad29bdbee75b654b
Viezas <dxviezas@gmail.com>
2022-06-17 12:46:16 +0200

    Announcement page

--- app/src/main/java/com/webstart/teamup/AnnouncementFragment.java
+++ app/src/main/java/com/webstart/teamup/AnnouncementFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SwipeFragment#newInstance} factory method to
+ * Use the {@link AnnouncementFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SwipeFragment extends Fragment {
+public class AnnouncementFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class SwipeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public SwipeFragment() {
+    public AnnouncementFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class SwipeFragment extends Fragment {
      * @return A new instance of fragment SwipeFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SwipeFragment newInstance(String param1, String param2) {
-        SwipeFragment fragment = new SwipeFragment();
+    public static AnnouncementFragment newInstance(String param1, String param2) {
+        AnnouncementFragment fragment = new AnnouncementFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,7 +59,7 @@ public class SwipeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_swipe, container, false);
+        return inflater.inflate(R.layout.fragment_announcements, container, false);
     }
 
     public void goNext(View view) {
