5372ce91d948b2423bc7fda7038770faf51afe50
Viezas <dxviezas@gmail.com>
2022-06-21 23:30:29 +0200

    Add recycler view to team information

---
+++ app/src/main/java/com/webstart/teamup/ClickMemberListener.java
@@ -0,0 +1,5 @@
+package com.webstart.teamup;
+
+public interface ClickMemberListener {
+    void onMemberClick(Structure_Profil_Min member);
+}
--- app/src/main/java/com/webstart/teamup/TeamActivity.java
+++ app/src/main/java/com/webstart/teamup/TeamActivity.java
@@ -1,10 +1,16 @@
 package com.webstart.teamup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -24,6 +30,19 @@ public class TeamActivity extends AppCompatActivity {
         Gson gson = new Gson();
         Structure_Team team = gson.fromJson(this.data, Structure_Team.class);
 
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView membersRecycler = this.findViewById(R.id.team_members_container);
+        membersRecycler.setHasFixedSize(true);
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(membersRecycler.getContext());
+        membersRecycler.setLayoutManager(manager);
+        RecyclerView.Adapter<TeamMemberAdapter.Holder> adapter = new TeamMemberAdapter(team.getMembers(),new ClickMemberListener(){
+            @Override
+            public void onMemberClick(Structure_Profil_Min member) {
+            }
+        }, this);
+        membersRecycler.setAdapter(adapter);
+        membersRecycler.setLayoutManager(layoutManager);
+
 //        TextView titleNote = findViewById(R.id.titleN);
 //        titleNote.setText(note.getTitle());
 //        TextView bodyNote = findViewById(R.id.bodyN);
@@ -34,4 +53,6 @@ public class TeamActivity extends AppCompatActivity {
         Intent profil = new Intent(this,ProfilActivity.class);
         startActivity(profil);
     }
+
+
 }
