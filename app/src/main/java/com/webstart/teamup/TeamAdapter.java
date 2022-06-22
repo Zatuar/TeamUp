2ccb8288e01b62b7ef6ef0b2fe1208778d7f8c9e
Viezas <dxviezas@gmail.com>
2022-06-22 11:19:03 +0200

    Team list member recycler

--- app/src/main/java/com/webstart/teamup/TeamAdapter.java
+++ app/src/main/java/com/webstart/teamup/TeamAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -26,12 +27,15 @@ public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.Holder> {
         TextView team_rank;
         TextView team_name;
         TextView team_score;
+        RecyclerView team_list_members_recycler;
 
         Holder(View itemView){
             super(itemView);
             team_rank = itemView.findViewById(R.id.team_rank);
             team_name = itemView.findViewById(R.id.team_name);
             team_score = itemView.findViewById(R.id.team_score);
+            team_list_members_recycler = itemView.findViewById(R.id.team_list_members_recycler);
+            team_list_members_recycler.setHasFixedSize(true);
         }
     }
 
@@ -45,8 +49,25 @@ public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.Holder> {
     @Override
     public void onBindViewHolder(@NonNull Holder holder, int position) {
         final Structure_Team key = teamsList.get(position);
-        holder.itemView.setOnClickListener(v -> listener.onTeamClick(key));
+        holder.team_name.setText(key.getName());
+        holder.team_score.setText(""+key.getScore()+"pts");
+        holder.team_rank.setText("#"+key.getRank());
+        holder.itemView.findViewById(R.id.team_header).setOnClickListener(v -> listener.onTeamClick(key));
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false);
+        holder.team_list_members_recycler.setLayoutManager(layoutManager);
+        RecyclerView.Adapter<TeamListMemberAdapter.Holder> adapter = new TeamListMemberAdapter(key.getMembers(),new ClickMemberListener(){
+            @Override
+            public void onMemberClick(Structure_Profil_Min member) {
+                selectedMember(member);
+            }
+        },this.context);
+        holder.team_list_members_recycler.setAdapter(adapter);
     }
+
+    private void selectedMember(Structure_Profil_Min member) {
+    }
+
     @Override
     public int getItemCount() {
         return teamsList.size();
